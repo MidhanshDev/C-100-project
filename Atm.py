a553c9698798943a017110fd537b256ef85f4bef
MidhanshDev <68544626+MidhanshDev@users.noreply.github.com>
2021-06-23 16:22:39 +0530

    Add files via upload

--- Atm.py
+++ Atm.py
@@ -4,9 +4,36 @@ class Atm(object):
        self.pinNumber= pinNumber
     
     def CashWithdrawl(self):
-        print("Enter amount")
+        amount = int(input("Enter amount"))
+        if(amount<10000):
+            newAmount = 10000-amount
+            print(f"You have withdrawn amount ${amount}.Your remaining balance is ${newAmount}")
+        else:
+            print("Insufficient balance")
     def BalanceEnquiry(self):
-        print("10000")
+        print("Your balance is $10000")
     def CashDeposit(self):
-        print("Enter amount")
+        amount = int(input("Enter amount"))
+        newAmount = 10000+amount
+        print(f"You have deposited ${amount}.Your new balance is ${newAmount}")
+
+def main():
+    cardNumber=input("Enter your card number")
+    pinNumber=input("Enter your pin number")
+    
+    newUser = Atm(cardNumber,pinNumber)
+    print("Choose your activity: 1 balance enquiry 2 withdrawl 3 deposit")
+    activity = int(input("Enter the activity number"))
+    if(activity==1):
+        newUser.BalanceEnquiry()
+    elif(activity==2):
+        newUser.CashWithdrawl()
+    elif(activity==3):
+        newUser.CashDeposit()
+    else:
+        print("Enter a valid option")
+
+if __name__=="__main__":
+    main()
+
     
